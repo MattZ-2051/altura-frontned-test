75885e54a3c97760ef2166e147f43258a411cec9
Matt Zamora <mzamora2899@gmail.com>
2023-03-07 15:41:36 -0700

    fix wagmi config and test contract call

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WagmiConfig, createClient } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { mainnet } from "wagmi/chains";
+import { configureChains } from "wagmi";
+
+const { chains, provider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: "QHoh9dK1mXOSm1WwbexcVItxu4SM83ml" })]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />{" "}
+    </WagmiConfig>
+  );
 }
---
+++ src/web3/contracts.ts
@@ -0,0 +1,6 @@
+import { contractAbi } from "./abi";
+
+export const nftContract = {
+  address: "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e",
+  abi: contractAbi,
+};
