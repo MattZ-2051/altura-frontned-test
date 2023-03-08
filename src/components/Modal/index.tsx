84e9dc1bda8368f4772e618ee6f0a24ed9499517
Matt Zamora <mzamora2899@gmail.com>
2023-03-08 16:13:46 -0700

    add common button component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ interface IProps {
 const Modal = ({ isOpen, setIsOpen, children }: IProps) => {
   return isOpen ? (
     <div className="absolute flex items-center justify-center w-full h-full transition-all bg-black bg-opacity-70">
-      <div className="w-[400px] h-[400px] bg-white rounded-lg  drop-shadow-2xl z-20">
+      <div className="w-[400px] h-[500px] bg-white rounded-lg  drop-shadow-2xl z-20">
         <div className="flex p-8 h-[inherit]">
           <XCircleIcon
             className="absolute w-6 h-6 hover:scale-110 top-4 right-8 hover:cursor-pointer"
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#A259FF",
+      },
+    },
   },
   plugins: [],
 };
